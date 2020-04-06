0adece1af9ae8de5a7f41cfe31d3d1875da0a992
trinishaV <62691029+trinishaV@users.noreply.github.com>
2020-04-06 19:27:28 +0200

    added student class

---
+++ test1/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Home Address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string Mobile { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
